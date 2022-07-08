565d7ecd66dcb86fb7d58187ed708f4d2f02478c
yury <82261418+chaplianski@users.noreply.github.com>
2022-07-08 11:59:39 +0300

    Update state

---
+++ app/src/main/java/com/example/lonia/domain/usecase/UpdateAnswerUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.lonia.domain.usecase
+
+import com.example.lonia.domain.model.Answers
+import com.example.lonia.domain.repository.AnswersRepository
+import javax.inject.Inject
+
+//class UpdateAnswerUseCase @Inject constructor(private val answersRepository: AnswersRepository) {
+//
+//    fun execute (answers: Answers){
+//        answersRepository.updateAnswer(answers)
+//    }
+//}
